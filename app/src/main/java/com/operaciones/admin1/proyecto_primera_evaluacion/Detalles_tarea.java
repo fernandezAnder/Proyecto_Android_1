8cb4931d61ea486481420cf0beb2dbc6b454f47e
Ander Fernandez <ander.fernandezla@elorrieta-errekamari.com>
2019-11-06 08:53:53 +0100

    1

---
+++ app/src/main/java/com/operaciones/admin1/proyecto_primera_evaluacion/Detalles_tarea.java
@@ -0,0 +1,14 @@
+package com.operaciones.admin1.proyecto_primera_evaluacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detalles_tarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_tarea);
+    }
+}
---
+++ app/src/main/java/com/operaciones/admin1/proyecto_primera_evaluacion/Lista_tareas.java
@@ -0,0 +1,14 @@
+package com.operaciones.admin1.proyecto_primera_evaluacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lista_tareas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_tareas);
+    }
+}
